16cc1721c802d4d69317d41209562ab42734cd01
Anna.Georgiadou <anna.georgiadou@autotrader.co.uk>
2022-08-19 15:15:56 +0100

    start on personalising app

---
+++ to-do-app/src/Modules/LearningComponents.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './LearningComponents.css'
+
+class LearningComponents extends Component {
+    render() {
+      return (
+        <div className='LearningComponent'>
+          bla
+        </div>
+      )
+    }
+  }
+
+  export default LearningComponents;
